75b384de115f07b8025644c8f8d8141be5171473
Manish Kumar <manish.engineer.tech.1582@gmail.com>
2026-02-24 22:26:08 -0500

    Initial production-ready FinRAG Engine structure

---
+++ rag-document-qa/src/pipeline.py
@@ -0,0 +1,10 @@
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from src.retriever import get_retriever
+from src.generator import build_qa_chain
+
+def load_pipeline():
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.load_local("vector_store", embeddings)
+    retriever = get_retriever(vectorstore)
+    return build_qa_chain(retriever)
